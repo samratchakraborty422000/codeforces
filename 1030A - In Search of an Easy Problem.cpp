999cc504d3140298309ed6b9259aa0c829d38b75
Samrat Chakraborty <121852717+samratchakraborty422000@users.noreply.github.com>
2023-12-03 10:02:00 +0530

    Create 1030A - In Search of an Easy Problem.cpp

---
+++ 1030A - In Search of an Easy Problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int i;
+        cin>>i;
+        if(i==1){
+            cout<<"HARD";
+            return 0;
+        }
+    }
+    cout<<"EASY";
+}
